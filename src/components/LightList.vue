b026801a644df0d895f4baa22fa58151c64a5b0d
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2022-06-18 13:36:20 +0200

    Take a handfull of lights to turn on/off on the same time

--- src/components/LightList.vue
+++ src/components/LightList.vue
@@ -11,11 +11,21 @@
     <v-row no-gutters>
       <v-container fluid class="navbar d-flex flex-row">
         <v-col cols="2">
-          <v-select v-model="lightShown" class="ma-0 pa-0" :items="lightStates" hide-details label="Vælg hvilke lys der skal vises"></v-select>
+          <v-select v-model="lightShown" class="ma-0 pa-0" :items="lightStates" hide-details label="Vælg hvilke lys der skal vises" />
+        </v-col>
+        <v-col offset="2" cols="4">
+          <v-text-field hide-details v-model="search" label="Søg blandt enheder" class="ma-0 pa-0" />
+        </v-col>
+        <v-col v-if="hideDefaultActionButtons" cols="4" class="d-flex align-center justify-end ma-0 pa-0">
+          <v-btn @click="toggleGroupState(false)" class="mx-1">Sluk gruppe</v-btn>
+          <v-btn @click="toggleGroupState(true)" class="mx-1">Tænd gruppe</v-btn>
         </v-col>
       </v-container>
     </v-row>
-    <v-data-table :items="filterLights" :headers="lightListHeaders">
+    <v-data-table :items="filterLights" :headers="lightListHeaders" :search="search">
+      <template #[`item.checkBox`]="{ item }">
+        <v-checkbox @click="toggleGroup(item)" />
+      </template>
       <template #[`item.state.on`]="{ item }">
         {{ item.state.on ? 'Tændt' : 'Slukket' }}
       </template>
@@ -23,8 +33,10 @@
         {{ item.state.xy ? 'XY : ' + item.state.xy : 'Hvid' }}
       </template>
       <template #[`item.actions`]="{ item }">
-        <v-btn class="mx-1" min-width="85px" @click="toggleLight(item)"> {{ item.state.on ? 'Sluk' : 'Tænd' }}</v-btn>
-        <v-btn class="mx-1" @click="showDetails(item)"> Details</v-btn>
+        <div v-show="!hideDefaultActionButtons">
+          <v-btn class="mx-1" min-width="85px" @click="toggleLight(item)"> {{ item.state.on ? 'Sluk' : 'Tænd' }}</v-btn>
+          <v-btn class="mx-1" @click="showDetails(item)"> Details</v-btn>
+        </div>
       </template>
     </v-data-table>
   </v-container>
@@ -38,6 +50,8 @@ export default {
     lights: [],
     lightShown: 0,
     fetching: false,
+    lightGroup: [],
+    search: '',
 
     lightStates: [
       {
@@ -55,6 +69,10 @@ export default {
     ],
 
     lightListHeaders: [
+      {
+        text: '',
+        value: 'checkBox',
+      },
       {
         text: 'Id',
         value: 'id',
@@ -92,6 +110,10 @@ export default {
     ],
   }),
   computed: {
+    hideDefaultActionButtons() {
+      if (this.lightGroup.length > 0) return true;
+      return false;
+    },
     filterLights() {
       if (this.lightShown == 1) {
         return this.lights.filter((x) => x.state.on == true);
@@ -102,6 +124,43 @@ export default {
     },
   },
   methods: {
+    // on = the state,
+    // if on = true; turn all lights on
+    // if on = false; turn all lights off
+    async toggleGroupState(on) {
+      if (on) {
+        for (let light of this.lightGroup) {
+          // console.log(`Turn on >> ${light.name} : ${light.id} : ${light.state.on}`);
+          if (light.state.on == false) {
+            // console.log(`Tænder ${light.name}`);
+            let response = await apiService.turnLightOn(light.id);
+            // console.log('Response >> ', response[0].success, response[0].success[Object.keys(response[0].success)[0]]);
+            light.state.on = response[0].success[Object.keys(response[0].success)[0]];
+            // console.log(light);
+          }
+        }
+      } else {
+        for (let light of this.lightGroup) {
+          // console.log(`Turn off >> ${light.name} : ${light.id} : ${light.state.on}`);
+          if (light.state.on == true) {
+            // console.log(`Slukker ${light.name}`);
+            let response = await apiService.turnLightOff(light.id);
+            // console.log('Response >> ', response[0].success, response[0].success[Object.keys(response[0].success)[0]]);
+            light.state.on = response[0].success[Object.keys(response[0].success)[0]];
+            // console.log(light);
+          }
+        }
+      }
+      this.getLights();
+    },
+
+    toggleGroup(light) {
+      if (this.lightGroup.some((l) => l.id === light.id)) {
+        this.lightGroup = this.lightGroup.filter((x) => x != light);
+      } else {
+        this.lightGroup.push(light);
+      }
+    },
     showDetails() {
       console.log('Details will be shown here.');
     },
